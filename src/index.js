ff2c0ff200876963cea2f8c77e3144dd15c24994
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-02-08 17:17:47 -0300

    Requisito 4

--- src/index.js
+++ src/index.js
@@ -25,6 +25,26 @@ function createToken(dataLogin) {
   return crypto.AES.encrypt(dataLogin, 'encrypted key').toString().substring(0, 16);
 }
 
+const validate = (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+
+  if (!(email.match(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/))) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+};
+
 app.get('/talker', async (_req, res) => {
   const talkers = await readTalker();
   return res.status(HTTP_OK_STATUS).json(talkers);
@@ -42,7 +62,7 @@ app.get('/talker/:id', async (req, res) => {
   });
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', validate, (req, res) => {
   const dataReq = JSON.stringify(req.body);
   const token = createToken(dataReq);
   return res.status(HTTP_OK_STATUS).json({ token });
