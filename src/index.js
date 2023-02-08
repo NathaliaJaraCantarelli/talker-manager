f60bfb4f54152baadcc901bfd5a611a6cd49c616
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-02-08 15:56:01 -0300

    Requisito 2

--- src/index.js
+++ src/index.js
@@ -11,14 +11,31 @@ const PORT = '3000';
 
 const talkersPath = path.resolve(__dirname, './talker.json');
 
-app.get('/talker', async (req, res) => {
+async function readTalker() {
   try {
     const talkers = await fs.readFile(talkersPath, 'utf-8');
-    return res.status(HTTP_OK_STATUS).json(JSON.parse(talkers));
+    return JSON.parse(talkers);
   } catch (error) {
     console.error('Não foi possível ler o arquivo talker.json');
   }
+}
+
+app.get('/talker', async (_req, res) => {
+  const talkers = await readTalker();
+  return res.status(HTTP_OK_STATUS).json(talkers);
 });
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readTalker();
+  const talkerId = talkers.filter((talker) => talker.id === Number(id));
+  if(talkerId.length > 0) {
+    return res.status(HTTP_OK_STATUS).json(talkerId[0]);
+  }
+  return res.status(404).json({
+    "message": "Pessoa palestrante não encontrada"
+  });
+})
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
